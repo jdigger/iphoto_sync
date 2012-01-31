849c4666141f636ca80e1c91cd849aba8dba0cdc
Jim Moore <moore.jim@gmail.com>
2012-01-30 23:21:21 -0700

    Adds Mongo support

--- script.rb
+++ script.rb
@@ -1,7 +1,6 @@
 #!/usr/local/bin/macruby
 
-require 'rubygems'
-require "sqlite3"
+REFRESH_ALL_DATA = false
 
 framework 'Foundation'
 framework 'ScriptingBridge'
@@ -9,8 +8,24 @@ framework 'ScriptingBridge'
 iphoto = SBApplication.applicationWithBundleIdentifier("com.apple.iphoto")
 load_bridge_support_file 'iPhoto.bridgesupport'
 
-#(iphoto.methods(true, true) - Object.new.methods(true, true)).sort
-
 photosAlbum = iphoto.photoLibraryAlbum
 
+#(iphoto.methods(true, true) - Object.new.methods(true, true)).sort
+#photosAlbum.photos[3926].properties
+
+#require "sqlite3"
 #db = SQLite3::Database.new('/Users/jmoore/Pictures/Jim\'s iPhoto Library/Database/Faces.db')
+
+require 'rubygems'
+require 'mongo'
+
+db = Mongo::Connection.new.db("iphoto")
+photo_data = db["photo_data"]
+
+if (REFRESH_ALL_DATA) then
+  photosAlbum.photos.each do |photo|
+    doc = { iphoto_id: photo.id, name: String.new(photo.name), longitude: photo.longitude, latitude: photo.latitude, altitude: photo.altitude,
+      height: photo.height, width: photo.width, comment: String.new(photo.comment), date: photo.date.utc, imagePath: String.new(photo.imagePath), }
+    photo_data.insert(doc)
+  end
+end
